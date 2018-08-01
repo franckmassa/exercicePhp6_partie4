c94993d02adede5ddee93136ccf7af9eb4224352
massa <massafranck@gmail.com>
2018-08-01 21:54:49 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
+<?php
+// Fonction qui renvoie une description avec 3 parametres
+  function returnDescription($lastname, $firstname, $age){
+    return 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . 'ans';
+  };
+?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice6</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Déclaration de la fonction identité et retourne la chaine de caractères
-    function identity($lastName, $firstName, $age){
-      return 'Bonjour ' . $lastName . ' ' .$firstName . ' ,tu as ' . $age . 'ans.';
-    }
-    //Affichage de la chaine de caractères
-    echo identity('Dupont', 'Martine', '25');
-    ?>
-  </p>
-</body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <p><?= returnDescription('Galland', 'Emmanuel', 24); ?></p>
+  </body>
 </html>
